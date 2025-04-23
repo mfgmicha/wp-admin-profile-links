84a35d6fa61a0a988160ead886c6283ca5a0503e
Micha Krapp <micha.krapp@sunlab.de>
2025-04-23 18:08:27 +0100

    feat: initial commit

---
+++ admin-profile-links.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name:     Admin Profile Links
+ * Plugin URI:      https://github.com/mfgmicha/wp-admin-profile-link
+ * Description:     Add links in the admin bar to your author page.
+ * Author:          Micha Krapp
+ * Author URI:      https://mfgmicha.de/
+ * Text Domain:     admin-profile-links
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ */
+
+class Admin_Profile_Links  {
+    public static function add_profile_links( \WP_Admin_Bar $wp_admin_bar ) {
+        $profile_link_data = [
+            'id'    => 'apl_show_profile_link',
+            'title' => __( 'View User', 'default' ),
+            'href'  => get_author_posts_url( get_current_user_id() ),
+            'meta'  => [
+                'target' => '_self',
+            ],
+        ];
+
+        // Add link to show user when on profile edit page.
+        if ( is_admin() && get_current_screen()?->id === 'profile' ) {
+            $wp_admin_bar->add_node( $profile_link_data );
+        }
+
+        // Add link to show user in user actions dropdown.
+        $wp_admin_bar->add_node(
+            array_merge(
+                $profile_link_data,
+                [
+                    'parent' => 'user-actions',
+                    'id' => 'apl_user-actions_show_profile_link'
+                ]
+            )
+        );
+    }
+}
+
+add_action( 'admin_bar_menu', [ Admin_Profile_Links::class, 'add_profile_links' ], 99 );
